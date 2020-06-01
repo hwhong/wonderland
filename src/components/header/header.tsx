d62c6353e1de8a5d53bf27856591f134e19c9d4e
warren <hwhong1129@gmail.com>
2020-05-31 21:50:19 -0700

    isastring

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./header.module.css";
 
 interface HeaderProps {
-  children: React.ReactNode;
+  children: string;
 }
 
 export function Header(props: HeaderProps) {
