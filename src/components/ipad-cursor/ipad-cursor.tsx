510d223034aa555a8f85d587652863f258414632
warren <hwhong1129@gmail.com>
2020-06-25 21:49:58 -0700

    why is it not showing up

--- src/components/ipad-cursor/ipad-cursor.tsx
+++ src/components/ipad-cursor/ipad-cursor.tsx
@@ -3,52 +3,51 @@ import styles from "./ipad-cursor.module.css";
 import classNames from "classnames";
 
 export function IpadCursor() {
-  const innerRef = React.useRef(null);
   const [isCursorLocked, setIsCursorLocked] = React.useState(false);
-  const cursor = document.querySelector("#cursor");
+  const cursorRef = React.useRef<HTMLDivElement>(null);
 
   const onMouseEnter = (e: React.MouseEvent<HTMLDivElement>) => {
     setIsCursorLocked(true);
     const target = e.currentTarget;
     const rect = target.getBoundingClientRect();
 
-    cursor.style.setProperty("--top", rect.top + rect.height / 2 + "px");
-    cursor.style.setProperty("--left", rect.left + rect.width / 2 + "px");
-    cursor.style.setProperty("--width", rect.width + "px");
-    cursor.style.setProperty("--height", rect.height + "px");
+    // cursor.style.setProperty("--top", rect.top + rect.height / 2 + "px");
+    // cursor.style.setProperty("--left", rect.left + rect.width / 2 + "px");
+    // cursor.style.setProperty("--width", rect.width + "px");
+    // cursor.style.setProperty("--height", rect.height + "px");
   };
 
   React.useEffect(() => {
-    const div = innerRef.current;
-    // subscribe event
-    div.addEventListener("scroll", handleOnScroll);
+    document.addEventListener("mousemove", onMouseMove);
     return () => {
-      // unsubscribe event
-      div.removeEventListener("scroll", handleOnScroll);
+      document.removeEventListener("mousemove", onMouseMove);
     };
   }, []);
 
-  const handleOnScroll = (e) => {
-    // NOTE: This is for the sake of demonstration purpose only.
-    // Doing this will greatly affect performance.
-    setScrollTop(e.target.scrollTop);
+  const onMouseMove = (e: MouseEvent) => {
+    const { pageX, pageY } = e;
+    if (cursorRef.current) {
+      cursorRef.current.setAttribute(
+        "style",
+        `transform: translate3d(${pageX}px, ${pageY}px, 0);`
+      );
+    }
   };
 
   return (
-    <div
-      className={styles.outer}
-      onMouseEnter={onMouseEnter}
-      onMouseLeave={() => setIsCursorLocked(false)}
-    >
+    <>
       <div
-        ref={innerRef}
-        className={classNames(styles.inner, {
-          [styles.selectedContent]: isCursorLocked,
-        })}
-      ></div>
-      <div id="cursor">
-        <div className="cursorContent"></div>
+        className={styles.outer}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={() => setIsCursorLocked(false)}
+      >
+        <div
+          className={classNames(styles.inner, {
+            [styles.selectedContent]: isCursorLocked,
+          })}
+        ></div>
       </div>
-    </div>
+      <div ref={cursorRef} className={styles.cursorContent}></div>
+    </>
   );
 }
