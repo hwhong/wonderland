0ff073d512b7f9fae9777bc42ad32b35af54ecbd
warren <hwhong1129@gmail.com>
2020-07-01 21:50:00 -0700

    sticky works

--- src/components/window-effect/window-effect.tsx
+++ src/components/window-effect/window-effect.tsx
@@ -3,8 +3,11 @@ import styles from "./window-effect.module.css";
 
 export function WindowEffect() {
   return (
-    <div className={styles.root}>
-      <div className={styles.content}></div>
-    </div>
+    <>
+      <div className={styles.root}>
+        <div className={styles.content}></div>
+      </div>
+      <div className={styles.overlay}></div>
+    </>
   );
 }
