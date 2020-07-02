702a7507ea2e8e076b919786ef5b6fc5218a167b
warren <hwhong1129@gmail.com>
2020-07-01 22:14:41 -0700

    getting super duper close

--- src/components/window-effect/window-effect.tsx
+++ src/components/window-effect/window-effect.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import styles from "./window-effect.module.css";
 
 export function WindowEffect() {
+  const windowRef = React.useRef<HTMLDivElement>(null);
+
+  React.useEffect(() => {
+    window.addEventListener("scroll", function (event) {});
+  }, []);
+
   return (
     <>
       <div className={styles.root}>
         <div className={styles.content}></div>
       </div>
       <div className={styles.overlay}>
-        <div className={styles.overlayContent}></div>
+        <div className={styles.window} ref={windowRef}></div>
       </div>
     </>
   );
