fef706878bfd2f2864ad0bd948adee6a24653fdb
warren <hwhong1129@gmail.com>
2020-07-05 22:25:13 -0700

    not working well ...

--- src/components/window-effect/window-effect.tsx
+++ src/components/window-effect/window-effect.tsx
@@ -16,7 +16,18 @@ export function WindowEffect() {
   return (
     <div className={styles.root}>
       <div className={styles.content}></div>
-      <div className={styles.window}> </div>
+      <div className={styles.window}>
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" fill="none">
+          <circle
+            cx="100"
+            cy="50"
+            r="40"
+            stroke="black"
+            stroke-width="2"
+            fill="none"
+          />
+        </svg>{" "}
+      </div>
     </div>
   );
 }
