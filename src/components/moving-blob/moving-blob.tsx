447102d7feb5f85e28ed98dc472a1c4eea3e1363
warren <hwhong1129@gmail.com>
2020-07-07 22:06:39 -0700

    coolio

--- src/components/moving-blob/moving-blob.tsx
+++ src/components/moving-blob/moving-blob.tsx
@@ -3,5 +3,10 @@ import styles from "./moving-blob.module.css";
 const path = require("./blob.svg");
 
 export function MovingBlob() {
-  return <img className={styles.root} src={path} />;
+  return (
+    <div className={styles.parent}>
+      <img className={styles.root} src={path} />
+      <div className={styles.blob}> </div>
+    </div>
+  );
 }
