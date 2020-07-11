d535795b1a563bc1f472b92c9e19f34db0afca4f
warren <hwhong1129@gmail.com>
2020-07-10 21:40:06 -0700

    more grid items

--- src/components/css-grid/css-grid.tsx
+++ src/components/css-grid/css-grid.tsx
@@ -4,7 +4,20 @@ import styles from "./css-grid.module.css";
 export function CSSGrid() {
   return (
     <div className={styles.root}>
-      <div className={styles.grid}></div>
+      <div className={styles.grid}>
+        <div className={styles.content}>1</div>
+        <div className={styles.content}>2</div>
+        <div className={styles.content}>3</div>
+        <div className={styles.content}>4</div>
+        <div className={styles.content}>5</div>
+        <div className={styles.content}>6</div>
+        <div className={styles.content}>7</div>
+        <div className={styles.content}>8</div>
+        <div className={styles.content}>9</div>
+        <div className={styles.content}>10</div>
+        <div className={styles.content}>11</div>
+        <div className={styles.content}>12</div>
+      </div>
     </div>
   );
 }
